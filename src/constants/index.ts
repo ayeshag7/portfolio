dc04888d957a41576c7729df5552ccfe7798354e
ayeshag7 <ayeshaiqbal2592@gmail.com>
2024-11-28 22:26:44 +0500

    Debugging images not loading - number

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,18 +1,11 @@
 export const technologies = [
+  { name: "Node JS", icon: "/portfolio/images/tech/nodejs.png" },
+  { name: "MongoDB", icon: "/portfolio/images/tech/mongodb.png" },
+  { name: "Three JS", icon: "/portfolio/images/tech/threejs.svg" },
   { name: "Docker", icon: "/portfolio/images/tech/docker.png" },
   { name: "TensorFlow", icon: "/portfolio/images/tech/TensorFlow.png" },
   { name: "OpenCV", icon: "/portfolio/images/tech/OpenCV.png" },
   { name: "Git", icon: "/portfolio/images/tech/git.png" },
-  { name: "HTML 5", icon: "/portfolio/images/tech/html.png" },
-  { name: "CSS 3", icon: "/portfolio/images/tech/css.png" },
-  { name: "JavaScript", icon: "/portfolio/images/tech/logo-javascript.svg" },
-  { name: "TypeScript", icon: "/portfolio/images/tech/typescript.png" },
-  { name: "React JS", icon: "/portfolio/images/tech/reactjs.png" },
-  { name: "Redux Toolkit", icon: "/portfolio/images/tech/redux.png" },
-  { name: "Tailwind CSS", icon: "/portfolio/images/tech/tailwind.png" },
-  { name: "Node JS", icon: "/portfolio/images/tech/nodejs.png" },
-  { name: "MongoDB", icon: "/portfolio/images/tech/mongodb.png" },
-  { name: "Three JS", icon: "/portfolio/images/tech/threejs.svg" },
   { name: "Figma", icon: "/portfolio/images/tech/figma.png" },
 ];
 
